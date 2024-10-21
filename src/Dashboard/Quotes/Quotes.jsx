9c1889010fb5bf0d0c62684e9a305b4ab17afbed
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-21 23:51:12 +0100

    Quotes component is done

--- src/Dashboard/Quotes/Quotes.jsx
+++ src/Dashboard/Quotes/Quotes.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Component } from "react";
+import QuotesAuthor from "../Components/QuotesAuthor";
+import QuotesText from "../Components/QuotesText";
 
-const Quotes = () => {
-  return (
-    <div>Quotes</div>
-  )
+class Quotes extends Component {
+  state = {
+    quote: "The Best Richness, is the Richness of Soul",
+    author: "Prophet Muhmmed(Peace be upon him)",
+    quotesData: [],
+    color: "rgb(243,156,18)",
+  };
+  render() {
+    return (
+      <div className="quote-box">
+        <QuotesText quote={this.state.quote} color={this.state.color}/>
+        <QuotesAuthor author={this.state.author} color={this.state.color}/>
+      </div>
+    );
+  }
 }
 
-export default Quotes
+export default Quotes;
