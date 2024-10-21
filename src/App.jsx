adc23234fe39a57534c5dbc69342991d0d65cd4e
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-21 23:42:33 +0100

    An update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
+import Buttons from './Dashboard/Components/Buttons';
+import Footer from './Dashboard/Components/Footer';
+import QuotesAuthor from './Dashboard/Components/QuotesAuthor';
+import QuotesText from './Dashboard/Components/QuotesText';
 import Quotes from './Dashboard/Quotes/Quotes';
 
 function App() {
   return (
     <div className="App">
       <Quotes/>
+      <Buttons/>
+      <Footer/>
+      <QuotesAuthor/>
+      <QuotesText/>
     </div>
   );
 }
