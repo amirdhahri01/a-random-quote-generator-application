04e446eb7fc40b2e6b852928af997b3058f9fa94
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-22 22:13:58 +0100

    Fetch quotes functionality is done

--- src/Dashboard/Quotes/Quotes.jsx
+++ src/Dashboard/Quotes/Quotes.jsx
@@ -1,7 +1,8 @@
 import { Component } from "react";
 import QuotesAuthor from "../Components/QuotesAuthor";
 import QuotesText from "../Components/QuotesText";
-import "./Quotes.css"
+import "./Quotes.css";
+import axios from "axios";
 class Quotes extends Component {
   state = {
     quote: "The Best Richness, is the Richness of Soul",
@@ -9,11 +10,38 @@ class Quotes extends Component {
     quotesData: [],
     color: "rgb(243,156,18)",
   };
+  randomColor = () => {
+    let colorPattrens = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += colorPattrens[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  fetchQuotes = () => {
+    axios
+      .get("https://quotes-api-self.vercel.app/quote")
+      .then((reponse) => {
+        const quote = reponse.data;
+        const color = this.randomColor();
+        document.body.style.color = color;
+        document.body.style.backgroundColor = color;
+        this.setState({
+          quote: quote.quote,
+          author: quote.author,
+          quotesData: [],
+          color: color,
+        });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   render() {
     return (
       <div className="quote-box">
-        <QuotesText quote={this.state.quote} color={this.state.color}/>
-        <QuotesAuthor author={this.state.author} color={this.state.color}/>
+        <QuotesText quote={this.state.quote} color={this.state.color} />
+        <QuotesAuthor author={this.state.author} color={this.state.color} />
       </div>
     );
   }
